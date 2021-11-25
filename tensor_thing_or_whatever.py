89c8694725cea9062eae5b9097941ddbea12f6dc
zerodevstuff <68030052+zerodevstuff@users.noreply.github.com>
2021-11-24 23:49:31 -0500

    main file
    idk tbh just the main python file we gonna use for this

---
+++ tensor_thing_or_whatever.py
@@ -0,0 +1,10 @@
+import tensorflow as tensor
+import tensorflow_hub as tfhub
+import pyautogui as pyau
+import win32api,win32gui,win32con
+import math
+import time
+import numpy as num
+import os
+import cv2
+#those are the imports i added some just incase i needed them but if i dont then ill remove them later
